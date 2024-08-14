cf7036999555cfce4a106607fc4c6f2061c9dc19
nedelweiss <edelweissnull@gmail.com>
2024-08-15 02:53:54 +0300

    Removed test comment

--- GView/MainWindow.xaml.cs
+++ GView/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using Microsoft.Win32;
 using TextBox = System.Windows.Controls.TextBox;
 using Timer = System.Threading.Timer;
 
-namespace GView; // test
+namespace GView;
 
 public partial class MainWindow : Window
 {
