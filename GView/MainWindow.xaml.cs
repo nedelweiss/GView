d5d664390961cda9e7e96f226ee0d150873993b7
nedelweiss <edelweissnull@gmail.com>
2024-08-15 02:47:20 +0300

    Removed redundant comment

--- GView/MainWindow.xaml.cs
+++ GView/MainWindow.xaml.cs
@@ -8,9 +8,6 @@ using Timer = System.Threading.Timer;
 
 namespace GView;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     private const string RegistryAppDir = @"SOFTWARE\GView";
